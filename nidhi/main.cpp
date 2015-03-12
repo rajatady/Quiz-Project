683b9dd682f1fb92027d899d87b57ad77d5e9c21
Kumar Divya Rajat <rajat.ady@gmail.com>
2015-03-12 13:14:50 +0530

    Initial Commit

---
+++ nidhi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nidhi
+//
+//  Created by Kumar Divya Rajat on 12/03/15.
+//  Copyright (c) 2015 Kumar Divya Rajat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
